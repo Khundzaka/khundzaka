baec7dd6c3c83c73dbd2e71361cfa800798e19b8
Khundzaka <a1a1a1a1>
2014-10-08 17:15:35 +0400

    cvlileba for iteratoris while ciklit

--- works/reducexelit.py
+++ works/reducexelit.py
@@ -1,9 +1,12 @@
 def plus_2(base, element):
     return element + base
 def reducexelit(func, sia):
-    axalisia = 0
-    for elementi in sia:
-        axalisia = func(axalisia, elementi)
+    axalisia = sia[0]
+    lengthof = len(sia) -1
+    d=1
+    while(d <= lengthof):
+        axalisia = func(axalisia, sia[d])
+        d+=1
     return axalisia
 chemisia = [2,3,4,5]
 print (reducexelit(plus_2, chemisia))
