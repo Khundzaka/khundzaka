038ffa590b72fbca8ccb5ee6b2cc5e99ecd29753
Khundzaka <alowap@mail.ru>
2014-10-15 20:28:37 +0400

    Update classes.py

--- works/classes.py
+++ works/classes.py
@@ -1,15 +1,15 @@
-class radio():
-   def __init__(self, name, channel, nowplaying, priceofmin):
+class Radio():
+   def __init__(self, name, channel, now_playing, priceofmin):
         """ """
         self.name = name
         self.channel = channel
-        self.nowplaying = nowplaying
+        self.now_playing = nowplaying
         self.priceofmin = priceofmin
 
    def rekl_calculator(self, days, mins):
         answer = self.priceofmin * days * mins
         return answer
 
-ardaidardo = radio("FortunaPlus", "103.4", "Justin Timberlake - Mirrors", 37.5)
+ardaidardo = Radio("FortunaPlus", "103.4", "Justin Timberlake - Mirrors", 37.5)
 print(ardaidardo.rekl_calculator(3,4))
-print("axla usment:\" %s \"arxze %s , %s" %(ardaidardo.nowplaying, ardaidardo.name, ardaidardo.channel))
+print("axla usment:\" %s \"arxze %s , %s" %(ardaidardo.now_playing, ardaidardo.name, ardaidardo.channel))
